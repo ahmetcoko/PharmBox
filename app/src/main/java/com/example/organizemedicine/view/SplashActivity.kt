e09059d0c191b9640823f2fb71d3013b0a002ecb
Ahmet Coko <ahmetcoko@gmail.com>
2024-06-02 14:47:47 +0300

    10th commit of the project

--- app/src/main/java/com/example/organizemedicine/view/SplashActivity.kt
+++ app/src/main/java/com/example/organizemedicine/view/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.organizemedicine.R
 
@@ -14,6 +16,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Find the ImageView
+        val logoImageView = findViewById<ImageView>(R.id.roundedImageView)
+
+        // Load the animation
+        val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+
+        // Start the animation
+        logoImageView.startAnimation(rotateAnimation)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MedicineFeedActivity::class.java))
